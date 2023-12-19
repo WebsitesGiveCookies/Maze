e21175ef949297215e315e04b4990a3a6aa5a7f3
WebsitesGiveCookies <154361757+WebsitesGiveCookies@users.noreply.github.com>
2023-12-19 19:29:30 +0200

    Add files via upload

---
+++ application.py
@@ -0,0 +1,12 @@
+#if you have a problem activating the Virtual environment:
+#run the following comment: 
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+#used to activate the VMaze:
+#.\VMaze\Scripts\activate
+from mazepy import mazepy
+
+grid = mazepy.Grid(10,10)
+grid = mazepy.getRandomMaze(grid)
+file = open("./maze.txt", "w")
+file.write("Madalina \n" + (str(grid)))
+file.close()
